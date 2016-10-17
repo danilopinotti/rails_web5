3c322e46ce50f7bd3f0cb03267ce295fa17038d3
Danilo Pinotti <danilopinotti@hotmail.com>
2016-10-17 21:03:44 +0000

    Filter routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users do
-    resources :contacts
+    resources :contacts do
+      resources :phones, only: [:index, :create, :new, :destroy]
+    end
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
